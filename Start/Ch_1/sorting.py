ec072bd0d7ecba181c57f7c0e84ee5a990ad577e
paul vincent <pverbrew@gmail.com>
2024-03-15 06:14:02 +0000

    completed sorting exercise used sorted on list and list attribute sort to sort in place sorted json quake data to get top 10 places and magnitude

--- Start/Ch_1/sorting.py
+++ Start/Ch_1/sorting.py
@@ -8,21 +8,32 @@ numbers = [42, 54, 19, 17, 23, 31, 16, 4]
 names = ["Jeff", "Bill", "Addie", "Stephanie", "Zach", "Lukas", "Joe", "Stacy"]
 
 # TODO: the sorted() function can be used to return a new list with sorted data
+print("use sorted for number array[42,54,19,17,23, 31, 16,4]\n", sorted(numbers))
+print('use sorted for names array: ["Jeff", "Bill", "Addie", "Stephanie", "Zach", "Lukas", "Joe", "stacy"]\n', sorted(names)) 
 
 
 # TODO: alternately, you can use the list object's sort() method, which sorts the list in-place
+numbers.sort(reverse=True)
+print("use reverse sort on number array[42,54,19,17,23, 31, 16,4]\n", numbers)
+names.sort(reverse=True)
+print('use reverse sort on names array: ["Jeff", "Bill", "Addie", "Stephanie", "Zach", "Lukas", "Joe", "stacy"]\n', names) 
 
 
 # TODO: To sort custom objects, we can tell the sort function which property to use
 # by specifying a key function
 
 # open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+with open("../../30DayQuakes.json", "r") as datafile:
+     data = json.load(datafile)
 
 
-# def getmag(dataitem):
-#     magnitude = dataitem["properties"]["mag"]
-#     if (magnitude is None):
-#         magnitude = 0
-#     return float(magnitude)
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
+
+data["features"].sort(key=getmag, reverse=True)
+for i in range(0,10):
+    print(data["features"][i]["properties"]["place"],
+          data["features"][i]["properties"]["mag"])
