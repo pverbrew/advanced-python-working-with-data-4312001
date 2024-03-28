2c42c381cc97ed809c2d725f73ceda75b492372f
paul vincent <pverbrew@gmail.com>
2024-03-28 16:17:46 -0700

    completed serialize csv exercise

--- Start/Ch_3/serialize_csv.py
+++ Start/Ch_3/serialize_csv.py
@@ -17,9 +17,48 @@ def isbig(x):
 
 # Filter the data by quakes that are larger than 5 magnitude
 largequakes = list(filter(isbig, data["features"]))
+#print(largequakes[0], "\n\n", largequakes[1], "\n\n")
+print("length of largequakes is", len(largequakes))
 
-# TODO: Create the header and row structures for the data
+#review: simplify largequakes
+def simp(data):
+    return {
+        "place"     : data["properties"]["place"], 
+        "magnitude" : data["properties"]["mag"],
+        "numreport" : data["properties"]["felt"]
+        }
+
+simpstruct = list(map(simp, largequakes))
+print("type for simpstruct is:", type(simpstruct))
+print("typr for elements in simpstruct is:", type(simpstruct[0]), "\n")
+print(simpstruct[0], "\n\n")
+
+
+
+
+
+#TODO: Create the header and row structures for the data
+header = ["Place", "Magnitude", "Date", "Link"]
+rows = []
 
 # TODO: populate the rows with the resulting quake data
+for quake in largequakes:
+    thedate = datetime.date.fromtimestamp(
+        int(quake["properties"]["time"])/1000)
+    rows.append([quake["properties"]["place"], quake["properties"]
+                ["mag"], thedate, quake["properties"]["url"]])
+
 
 # TODO: write the results to the CSV file
+print("NOTE: csv module writerow NO ENDLINE, writerows does ENDLINE at each row")
+with open("largequakes.csv", "w", newline='', encoding="utf-8") as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(header)
+    writer.writerows(rows)
+
+
+print("type for largequakes is:", type(largequakes))
+print("type for quake is:", type(quake), "\n")
+#print("type for quake[0] is:", type(quake[0]), "\n")
+print("type for rows is:", type(rows), "\n")
+print("type for rows[0] is:", type(rows[0]), "\n")
