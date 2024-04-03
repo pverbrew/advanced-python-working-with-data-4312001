f1eecf66746977b4a6a72cc1d2c0cf605a07fc82
paul vincent <pverbrew@gmail.com>
2024-04-03 11:22:08 -0700

    added code to basiclog_start.py

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,18 @@
 # demonstrate the logging api in Python
 
 # TODO: use the built-in logging module
-
+import logging
 
 # TODO: Use basicConfig to configure logging
-
+logging.basicConfig(filename="basiclog.log",
+                    level= logging.DEBUG)
 # TODO: Try out each of the log levels
-
+logging.debug("logging.debug message")
+logging.info("logging.info message")
+logging.warning("logging.warning message")
+logging.error("logging.error message")
+logging.critical("logging.critical message")
 # TODO: Output formatted strings to the log
+fmtstr= f"%(timedate)s  %(function)s"
+
 
